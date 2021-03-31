883edca5223e2d911afb18e4409c8b8bd91e03b6
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2021-03-31 20:53:17 +0530

    Create basicabhi9.py

---
+++ basicabhi9.py
@@ -0,0 +1,11 @@
+'''10. Program to input two numbers and print the swapped values of them.'''
+a = float(input("Enter the first number"))
+b = float(input("Enter the second number"))
+c=0 
+c = b 
+b = a 
+a = c 
+print("The first number after swapping is:",end=' ')
+print(a)
+print("The second number after swapping is:",end=' ')
+print(b)
