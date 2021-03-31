3114abe0f51aa4ca3b36e7d6cff04c48955831eb
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2021-03-31 19:52:58 +0530

    Create basicabhi5.py

---
+++ basicabhi5.py
@@ -0,0 +1,5 @@
+'''6. Program to input two integer values and calculate first number raised to the power second
+number.'''
+a = int(input("Enter a number"))
+b = int(input("Enter the second number"))
+print(a**b)
